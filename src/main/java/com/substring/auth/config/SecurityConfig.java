1ffd0a5138295489d3046e609a379e7c8d522a28
krishanamondal <mail.krishanamondal@gmail.com>
2026-05-03 06:45:47 +0600

    adding spring security feture via in memory userDetails

--- src/main/java/com/substring/auth/config/SecurityConfig.java
+++ src/main/java/com/substring/auth/config/SecurityConfig.java
@@ -6,12 +6,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public UserDetailsService user(){
-        UserDetails user = User.
+        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+
+        UserDetails user1 = userBuilder.username("pujus").password("jus").roles("ADMIN").build();
+        UserDetails user2 = userBuilder.username("john").password("doe").roles("USER").build();
+    return new InMemoryUserDetailsManager(user1, user2);
     }
 }
