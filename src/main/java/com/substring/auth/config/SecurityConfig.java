1c34cff03bfd1c9c00ef34a345b91d8cf3a1454d
krishanamondal <mail.krishanamondal@gmail.com>
2026-05-20 23:48:31 +0600

    now are added many  code but not need

--- src/main/java/com/substring/auth/config/SecurityConfig.java
+++ src/main/java/com/substring/auth/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(
             AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
-        // add 
+        // add
     }
 
     @Bean
